30ad1909b1005b7778f6a34d887d2f0d3621bf4a
Spencer Harmon <spencerharmon.91@gmail.com>
2022-10-17 00:49:40 -0500

    receive messages from st-sync client

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,16 @@
 use st_sync;
 use tokio;
+use crossbeam_channel::*;
 
 #[tokio::main]
 async fn main() {
-    let sync_client = st_sync::client::Client::new();
+    let (tx, rx) = bounded(1);
+    let sync_client = st_sync::client::Client::new(tx);
+    tokio::spawn(async move {
+	loop {
+	    println!("{:?}", rx.recv().unwrap());
+	}
+    });
     match sync_client.start().await {
 	Ok(()) => (),
 	Err(message) => println!("{}", message)
